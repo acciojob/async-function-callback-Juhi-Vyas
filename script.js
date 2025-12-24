a2388c563e56c079b67df1d907c1f1426715936c
Juhi-Vyas <125583903+Juhi-Vyas@users.noreply.github.com>
2025-12-25 02:41:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+let btn = document.getElementById("btn")
+let output = document.getElementById("output")
 
-//your JS code here. If required.
+
+btn.addEventListener("click", ()=>{
+	fetch(url)
+	.then(res=>res.json())
+	.then(data=>{
+		output.textContent = data.title
+	})
+	
+})
+
+
+
